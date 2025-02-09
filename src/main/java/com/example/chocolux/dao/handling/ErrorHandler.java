a3a2a02130a543226fb885e171ba2c7305d01b27
Bahadir-coder <bahadurmusayev15@gmail.com>
2025-02-09 15:05:05 +0400

    ChocoLux

--- src/main/java/com/example/chocolux/dao/handling/ErrorHandler.java
+++ src/main/java/com/example/chocolux/dao/handling/ErrorHandler.java
@@ -3,10 +3,17 @@ package com.example.chocolux.dao.handling;
 import com.example.chocolux.dao.exception.FoundException;
 import com.example.chocolux.dao.exception.NotFoundException;
 import com.example.chocolux.model.ExceptionDto;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -20,5 +27,36 @@ public class ErrorHandler {
     @ExceptionHandler(FoundException.class)
     public ExceptionDto foundHandler(RuntimeException exception){
         return new ExceptionDto(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(violation -> {
+            String fieldName = violation.getPropertyPath().toString();
+            String errorMessage = violation.getMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                body("Internal server error occurred: " + ex.getMessage());
     }
 }
